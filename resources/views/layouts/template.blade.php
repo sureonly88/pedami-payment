0905e4bbaa836c4ecb0ec7300d520f43b0cbbfc0
Yakin <sureonly88@gmail.com>
2025-09-28 09:24:29 +0800

    test

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,21 +6,22 @@
   <title>Pedami Payment | Billing</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="{{ URL::asset('plugins/select2/select2.min.css') }}">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="stylesheet" href="{{ secure_asset('plugins/select2/select2.min.css') }}">
   <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="{{ URL::asset('bootstrap/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('bootstrap/css/bootstrap.min.css') }}">
   <!-- Font Awesome -->
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css"> -->
   <!-- Ionicons -->
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"> -->
   <!-- Theme style -->
 
-  <link rel="stylesheet" href="{{ URL::asset('font-awesome/css/font-awesome.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('ionicons/css/ionicons.min.css') }}">
-  <link rel="stylesheet" href="{{ URL::asset('dist/css/AdminLTE.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('font-awesome/css/font-awesome.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('ionicons/css/ionicons.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('dist/css/AdminLTE.min.css') }}">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="{{ URL::asset('dist/css/skins/_all-skins.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('dist/css/skins/_all-skins.min.css') }}">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -28,34 +29,34 @@
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-  <link rel="stylesheet" href="{{ URL::asset('jquery-ui/jquery-ui.min.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('jquery-ui/jquery-ui.min.css') }}">
 
-  <link rel="stylesheet" href="{{ URL::asset('styles/dataTables.bootstrap.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('styles/dataTables.bootstrap.css') }}">
 
-  <script src="{{ URL::asset('plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
-  <script src="{{ URL::asset('jquery-ui/jquery-ui.min.js') }}"></script>
+  <script src="{{ secure_asset('plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+  <script src="{{ secure_asset('jquery-ui/jquery-ui.min.js') }}"></script>
 
-  <script src="{{ URL::asset('js/jqmq.js') }}"></script>
-  <script src="{{ URL::asset('plugins/select2/select2.full.min.js') }}"></script>
-  <script src="{{ URL::asset('js/jquery.dataTables.min.js') }}"></script>  
-  <script src="{{ URL::asset('js/dataTables.bootstrap.min.js') }}"></script>
-  <script src="{{ URL::asset('js/numeral.min.js') }}"></script>
+  <script src="{{ secure_asset('js/jqmq.js') }}"></script>
+  <script src="{{ secure_asset('plugins/select2/select2.full.min.js') }}"></script>
+  <script src="{{ secure_asset('js/jquery.dataTables.min.js') }}"></script>  
+  <script src="{{ secure_asset('js/dataTables.bootstrap.min.js') }}"></script>
+  <script src="{{ secure_asset('js/numeral.min.js') }}"></script>
 
   @if (env('APP_DEBUG'))
-  <script src="{{ URL::asset('vuejs/vue.js') }}"></script>
+  <script src="{{ secure_asset('vuejs/vue.js') }}"></script>
   @else
-  <script src="{{ URL::asset('vuejs/vue.min.js') }}"></script>
+  <script src="{{ secure_asset('vuejs/vue.min.js') }}"></script>
   @endif
-  <script src="{{ URL::asset('js/axios.min.js') }}"></script>
-  <script src="{{ URL::asset('js/linq.min.js') }}"></script>
+  <script src="{{ secure_asset('js/axios.min.js') }}"></script>
+  <script src="{{ secure_asset('js/linq.min.js') }}"></script>
 
 <!--   <script src="https://unpkg.com/vue"></script> -->
 <!--   <script src="https://unpkg.com/axios/dist/axios.min.js"></script> -->
 <!--   <script src="https://cdn.jsdelivr.net/vue2-filters/0.1.7/vue2-filters.min.js"></script>
   <script src="https://cdn.jsdelivr.net/vue.resource/1.2.1/vue-resource.min.js"></script> -->
 
-  <script src="{{ URL::asset('js/vue2-filters.min.js') }}"></script>
-  <script src="{{ URL::asset('js/vue-resource.min.js') }}"></script>
+  <script src="{{ secure_asset('js/vue2-filters.min.js') }}"></script>
+  <script src="{{ secure_asset('js/vue-resource.min.js') }}"></script>
 
   <script>
       $.ajaxSetup({
@@ -110,7 +111,7 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="{{ URL::asset('dist/img/avatar04.png') }}" class="img-circle" alt="User Image">
+          <img src="{{ secure_asset('dist/img/avatar04.png') }}" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p>{{ strtoupper($user['username']) }} - {{ strtoupper($user['role']) }}</p>
@@ -158,28 +159,28 @@
 <!-- ./wrapper -->
 
 <!-- Js untuk hasil Laravel Elixir -->
-<!-- <script src="{{ URL::asset('js/app.kopkar.js') }}"></script> -->
+<!-- <script src="{{ secure_asset('js/app.kopkar.js') }}"></script> -->
 
-<script src="{{ URL::asset('bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ secure_asset('bootstrap/js/bootstrap.min.js') }}"></script>
 <!-- SlimScroll -->
-<script src="{{ URL::asset('plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
+<script src="{{ secure_asset('plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
 <!-- FastClick -->
-<script src="{{ URL::asset('plugins/fastclick/fastclick.js') }}"></script>
+<script src="{{ secure_asset('plugins/fastclick/fastclick.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ URL::asset('dist/js/app.min.js') }}"></script>
+<script src="{{ secure_asset('dist/js/app.min.js') }}"></script>
 
-<link rel="stylesheet" href="{{ URL::asset('css/animate.css') }}">
-<script src="{{ URL::asset('js/bootstrap-notify.min.js') }}"></script>
+<link rel="stylesheet" href="{{ secure_asset('css/animate.css') }}">
+<script src="{{ secure_asset('js/bootstrap-notify.min.js') }}"></script>
 
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.3/socket.io.min.js"></script> -->
-<script src="{{ URL::asset('js/socket.io.min.js') }}"></script>
+<script src="{{ secure_asset('js/socket.io.min.js') }}"></script>
 
-<script src="{{ URL::asset('app/kopkar.js') }}"></script>
+<script src="{{ secure_asset('app/kopkar.js') }}"></script>
 
-<script src="{{ URL::asset('printing/dependencies/rsvp-3.1.0.min.js') }}"></script>
-<script src="{{ URL::asset('printing/dependencies/sha-256.min.js') }}"></script>
-<script src="{{ URL::asset('printing/qz-tray.js') }}"></script>
-<script src="{{ URL::asset('printing/print_java.js') }}"></script>
+<script src="{{ secure_asset('printing/dependencies/rsvp-3.1.0.min.js') }}"></script>
+<script src="{{ secure_asset('printing/dependencies/sha-256.min.js') }}"></script>
+<script src="{{ secure_asset('printing/qz-tray.js') }}"></script>
+<script src="{{ secure_asset('printing/print_java.js') }}"></script>
 
 <script type="text/javascript">
 $(document).ready(function() {
